6c999408adba02a1536b15eadacf4770cbf8142a
Adrianocorreia-025 <82658411+Adrianocorreia-025@users.noreply.github.com>
2021-05-20 21:40:04 +0100

    Criadas as Funcoes para tabela de Curado por covid Por dia

---
+++ app/src/main/java/ar/adriano/bdadospacientescovid/TCuradosPorDia.kt
@@ -0,0 +1,50 @@
+package ar.adriano.bdadospacientescovid
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TCuradosPorDia(db: SQLiteDatabase) {
+    private val db: SQLiteDatabase = db
+    fun criaTabelas() {
+        // Criar tabelas dentro da base de Dados
+        db.execSQL("CREATE TABLE " + NOME_TABLE + "(" + BaseColumns._ID + "INTEGER PRIMARY KEY AUTOINCREMENT," + CABECALHO + " TEXT UNIQUE," + NOME_PACIENTE + " TEXT UNIQUE," + ID_TPACINTE + " INTEGER NOT NULL," + " FOREIGN KEY" + "(id_TPaciente)" + " REFERENCES TPaciente" + ")")
+
+    }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(TabelaPacientes.NOME_TABLE, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(TabelaPacientes.NOME_TABLE, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(TabelaPacientes.NOME_TABLE, whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String,
+        selectionArgs: Array<String>,
+        groupBy: String,
+        having: String,
+        orderBy: String
+    ): Cursor? {
+        return db.query(TabelaPacientes.NOME_TABLE, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
+
+
+    companion object
+    {
+        const val NOME_TABLE="TPacienteSInfectPorDia"
+        const val  CABECALHO = " PacientesInfectados"
+        const val NOME_PACIENTE = "NomePaciente"
+        const val ID_TPACINTE =  "id_TPaciente"
+
+    }
+
+}
