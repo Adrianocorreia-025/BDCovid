1b6f210010dd40a539bb4697f6afd1e1b5f483dc
Adrianocorreia-025 <82658411+Adrianocorreia-025@users.noreply.github.com>
2021-05-20 22:15:42 +0100

    CRIADA A FUNCAO PARA APAGAR BD ANTES DE EXECUTAR

--- app/src/androidTest/java/ar/adriano/bdadospacientescovid/TestarBDados.kt
+++ app/src/androidTest/java/ar/adriano/bdadospacientescovid/TestarBDados.kt
@@ -7,17 +7,25 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+// tesyat Base de dados se pode ou nao Abre
 @RunWith(AndroidJUnit4::class)
 class TestarBDados {
 
     private fun getContex() = InstrumentationRegistry.getInstrumentation().targetContext
 
+    @Before
+
+    fun cleanBD () {
+        getContex().deleteDatabase(BDPCovidOpenHelper.NOME_BASE_DADOS)
+    }
     @Test
     fun AbrirBDPacientes(){
        val AbrirBD=  BDPCovidOpenHelper(getContex())
