cb75678afbbe8fc8407153cec469c9687a5114bc
Adrianocorreia-025 <82658411+Adrianocorreia-025@users.noreply.github.com>
2021-05-20 21:10:25 +0100

    Criadas os Atributos para a tabela Infectados Por dia

--- app/src/main/java/ar/adriano/bdadospacientescovid/TabelaPacientes.kt
+++ app/src/main/java/ar/adriano/bdadospacientescovid/TabelaPacientes.kt
@@ -7,8 +7,14 @@ class TabelaPacientes(db: SQLiteDatabase?) {
     private val  db: SQLiteDatabase? = db
     fun criaTabelas() {
         // Criar tabelas dentro da base de Dados
-     db?.execSQL("CREATE TABLE TPaciente(_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT UNIQUE)")
+     db?.execSQL("CREATE TABLE " + NOME_TABLE + "(" + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + Nome +"TEXT UNIQUE" + ")")
 
     }
 
+
+    companion object{
+
+        const val NOME_TABLE = "TPaciente"
+        const val Nome ="nome"
+    }
 }
--- app/src/main/java/ar/adriano/bdadospacientescovid/TabelaPacinttesInfPorDia.kt
+++ app/src/main/java/ar/adriano/bdadospacientescovid/TabelaPacinttesInfPorDia.kt
@@ -1,12 +1,21 @@
 package ar.adriano.bdadospacientescovid
 
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 
 class TabelaPacinttesInfPorDia(db: SQLiteDatabase?) {
     private val  db: SQLiteDatabase? = db
     fun criaTabelas() {
         // Criar tabelas dentro da base de Dados
-        db?.execSQL("CREATE TABLE TPacienteSInfectPorDia(_id INTEGER PRIMARY KEY AUTOINCREMENT, PacientesInfectados TEXT UNIQUE,NomePaciente TEXT UNIQUE,id_TPaciente INTEGER NOT NULL, FOREIGN KEY(id_TPaciente) REFERENCES TPaciente)")
+        db?.execSQL("CREATE TABLE " + NOME_TABLE + "(" + BaseColumns._ID + "INTEGER PRIMARY KEY AUTOINCREMENT," + CABECALHO + " TEXT UNIQUE," + NOME_PACIENTE + " TEXT UNIQUE," + ID_TPACINTE + " INTEGER NOT NULL," + " FOREIGN KEY" + "(id_TPaciente)" + " REFERENCES TPaciente" + ")")
+
+    }
+    companion object
+    {
+        const val NOME_TABLE="TPacienteSInfectPorDia"
+        const val  CABECALHO = " PacientesInfectados"
+        const val NOME_PACIENTE = "NomePaciente"
+        const val ID_TPACINTE =  "id_TPaciente"
 
     }
 
